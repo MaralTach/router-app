a2929be928bf4168b05dc58158206f86f128f073
MaralTach <yoloten.maral@gmail.com>
2024-04-09 00:40:05 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Instructors from "./pages/Instructors";
-import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import Paths from "./pages/Paths"
+import PersonDetail from "./pages/PersonDetail"
+import NotFound from "./pages/NotFound"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   <>
     <BrowserRouter>
-    <Nav />
-    <Routes>
-      <Route path="/" element={ <Home />}/>
-      <Route path="/" element={<Instructors />}/>
-      <Route path="/" element={<Contact />}/>
-      <Route path="*" element={<NotFound />}/>
-    
-    </Routes>
-   
-    <Footer />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/paths" element={<Paths />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
-  
-   </>
-   
-  
   )
 }
 
-export default App;
+export default App
